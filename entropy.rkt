7567ca835597772ea65e59c46f2ca6a58cf178a4
Nate Rosenbloom <nrosenbloom@brandeis.edu>
2014-09-29 01:01:46 -0400

    finished the make-dec-tree just need to make sure best-classifies is working

--- entropy.rkt
+++ entropy.rkt
@@ -50,13 +50,15 @@
 
 
 (define (all-positive classes)
-  (all-the-same #t value))
+  (all-the-same #t classes))
+
 (define (all-negative classes)
-  (all-the-same #f value))
+  (all-the-same #f classes))
+
 (define (all-the-same value list)
   (if (null? list)
       #t
-      (if (not eq? (car classes) value)
+      (if (not (eq? (car list) value))
 	  #f
 	  (all-the-same (cdr list) value))))
 
@@ -78,41 +80,56 @@
   (common-iter classes '()))
 
 (define (best-classifies attributes examples)
-  foldl (lambda (x acc)
+  (foldl (lambda (x acc)
 	  (let ((gains (gain x examples)))
 	    (if (> gains (car acc))
 		(cons gains x)
-		acc))))
-(define (values attribute examples)
+		acc))) '() '())) ;;todo
+
+(define (list-values attribute examples)
   (foldl (lambda (x acc)
 	   (if (member (attribute x) acc)
 	       (cons (attribute x) acc)
 	       acc))))
+(define (filter-on-attribute attribute examples)
+  (define (filter-iter hash examples)
+    (if (null? examples) hash
+	(let ((value (attribute (car examples))))
+	  (filter-iter
+	   (hash-set! hash value (cons (car examples) (hash-ref! hash value '())))
+	   (cdr examples)))))
+  (filter-iter (make-hash) examples))
 
 (define (make-dec-tree classifier attributes examples)
-  (let ((classes (map classifier attributes)))
+  (let ((classes (map classifier examples)))
     (cond ((all-positive classes)
 	   't)
 	  ((all-negative classes)
 	   'f)
 	  ((null? attributes) (most-common-value classes))
-	  (else (let ((attrib (cdr (best-classifies attribues examples))))
-		  (map )))))
+	  (else (let ((attrib (cdr (best-classifies attributes examples))))
+		  (cons attrib (let ((remaining-attributes (remove attrib attributes))
+				     (values (list-values attrib examples))
+				     (value-hash (filter-on-attribute attrib examples)))
+				 (map (lambda (value) ((let ((remaining-examples (hash-ref value-hash value)))
+							 (if (null? remaining-examples)
+							     (most-common-value classes)
+							     (cons value
+								   (make-dec-tree classifier
+										  remaining-attributes
+										  remaining-examples)))))) values))))))))
 
 
 
-
-
-
-
-  (define test-examples (list (cons 't 'weak) (cons 't 'weak) (cons 't 'weak)
-			      (cons 't 'weak) (cons 't 'weak) (cons 't 'weak)
-			      (cons 't 'strong) (cons 't 'strong) (cons 't 'strong)
-			      (cons 'f 'weak) (cons 'f 'weak) (cons 'f 'strong)
-			      (cons 'f 'strong) (cons 'f 'strong))))
+(define test-examples (list (cons 't 'weak) (cons 't 'weak) (cons 't 'weak)
+			    (cons 't 'weak) (cons 't 'weak) (cons 't 'weak)
+			    (cons 't 'strong) (cons 't 'strong) (cons 't 'strong)
+			    (cons 'f 'weak) (cons 'f 'weak) (cons 'f 'strong)
+			    (cons 'f 'strong) (cons 'f 'strong)))
 
 (define (wind-strength x) (cdr x))
 
-(- (entropy class test) (gain wind-strength test))
+;;(- (entropy class test) (gain wind-strength test))
 
-(make-dec-tree class (list wind-strength) test-examples)
+					(make-dec-tree class (list wind-strength) test-examples)
+
