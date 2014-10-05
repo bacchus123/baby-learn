74c9249f69dee4977ffc9055627b71c93f994f24
Nate Rosenbloom <nrosenbloom123@gmail.com>
2014-10-05 03:03:19 -0400

    changed model

--- titanic.rkt
+++ titanic.rkt
@@ -1,4 +1,6 @@
 #lang racket
+
+(provide titanic-tree)
 (require racket/runtime-path)
 (require (planet neil/csv))
 (require "decision-tree.rkt")
@@ -16,7 +18,7 @@
       (let ((row (reader)))
 	(if (null? row)
 	    '()
-	    (cons row (loop)))))   (loop)))
+	    (cons row (loop)))))  (loop)))
 
 
 (define training-set  (get-rows (make-csv-reader (open-input-file titanic-train))))
@@ -26,17 +28,28 @@
 (define (class sample) (list-ref sample 2))
 (define (sex sample) (list-ref sample 4))
 (define (age sample) (let ((age (string->number (list-ref sample 5))))
-		       (cond ((eqv? age #f) -1)
-			     ((< age 10) 1)
+		       (cond ((eqv? age #f) 0)
+			     ((< age 18) 1)
 			     (else 0))))
+
+(define (sib sample) (let ((sib (string->number (list-ref sample 6))))
+		       (if (> sib 7) 7 sib)))
 (define (pass-id sample) (list-ref sample 0))
+
+(define (fare sample) (let ((fare (string->number (list-ref sample 9))))
+			(cond ((eq? fare #f) 0)
+			      ((< fare 10) 1)
+			      ((< fare 20) 2)
+			      ((< fare 30) 3)
+			      (else 4))))
+
+(define (embarked sample) (list-ref sample) 11)
 (define titanic-tree
-  (make-tree survives (list class age sex) training-set))
-
-
-(for-each (lambda (x) (begin  (write (car x))
-			 (display  " , ")
-			 (write  (cdr x))
-			 (newline)))
-     (map (lambda (x) (cons (pass-id x) (run-tree titanic-tree (cons '() x)))) test-set))
-
+  (make-tree survives (list class sex fare) training-set))
+titanic-tree
+;; (for-each (lambda (x)
+;; 	    (begin  (display (car x))
+;; 		      	 (display  ",")
+;; 		      	 (display (cdr x))
+;; 		      	 (newline)))
+;;      (map (lambda (x) (cons (pass-id x) (run-tree titanic-tree (cons '() x)))) test-set))
